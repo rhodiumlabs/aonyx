92e2e7f3b10a8d49bd77030e0e58335d93fd513e
nadim <i.nadim@gmail.com>
2016-12-14 23:05:34 -0500

    add fonts

--- containers/page.js
+++ containers/page.js
@@ -10,8 +10,7 @@ export default class extends React.Component {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.1.0/milligram.min.css" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,200,300,500,600" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Bungee+Hairline|Poiret+One|Rajdhani|Source+Sans+Pro" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Work+Sans:800" rel="stylesheet" />
       </Head>
       <main className={"wrapper "+this.props.className}>
         {this.props.children}
