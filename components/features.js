d8340a49a1bdc0fb9bbdc88bcc2abdba1633d35c
alexandra-rhodium <alexandra@rhodium.io>
2016-12-14 21:21:40 -0500

    alexchanges4
    WOW

--- components/features.js
+++ components/features.js
@@ -37,40 +37,35 @@ export default () => (
         <div className="column column-33" style={{background: blue}}>
           <div>
             <h3 className={headerCard}>Reinsurance</h3>
-            <h4>Internet of things</h4>
-            <p>Today, we are here to bring 
-            forth the new. Our 
-            technology creates new 
-            systems to make public and 
-            private institutions work 
-            together in unison 
-            for the betterment of humanity</p>
+            <h4>Collaboration made simple</h4>
+            <p>Shared data and better transparency between all stakeholders, through the use of smart contracts and decentralized 
+  applications. Data can be validated by any authorized participant on the network, 
+  therefore insurance policies and associated claim information is on a trusted network. 
+  Event details are documented using cryptographic methods, resulting in a tamper-proof record of 
+  all relevant information for the creation of higher quality policies and faster claim payout.
+</p>
           </div>
         </div>
         <div className="column column-33" style={{background: yellow}}>
           <div>
             <h3 className={headerCard}>Peer to peer economy</h3>
-            <h4>Internet of things</h4>
-            <p>Today, we are here to bring 
-            forth the new. Our 
-            technology creates new 
-            systems to make public and 
-            private institutions work 
-            together in unison 
-            for the betterment of humanity</p>
+            <h4>Fast, smart movement</h4>
+            <p>Using decentralized applications and smart contracts  catastrophic events can trigger payments 
+  through Aonyx to parties, vendors and private assistance for those in need. 
+  When an event occurs that meets predefined conditions, Aonyx  accesses policyholders 
+  data sources on the blockchain, then determine and activate payouts to and from contract parties.</p>
           </div>
         </div>
         <div className="column column-33" style={{background: blue}}>
           <div>
             <h3 className={headerCard}>Smart Cities</h3>
             <h4>Internet of things</h4>
-            <p>Today, we are here to bring 
-            forth the new. Our 
-            technology creates new 
-            systems to make public and 
-            private institutions work 
-            together in unison 
-            for the betterment of humanity</p>
+            <p>Information capturing devices and smart contracts, Aonyx will be able to  prompt emergency services 
+  when needed. By capturing data onto the immutable ledger— 
+  this information will save time and resources on when is the right time to act. 
+  Imagine WiFi drones, connected weather networks giving the most up to date data to citizens, 
+  public workers, government offices and insurance companies when help is needed most.
+</p>
           </div>
         </div>
       </div>
